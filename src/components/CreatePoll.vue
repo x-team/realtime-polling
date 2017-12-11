8b9fdfb7aff5c81c696d034e42f49a17d6b2095f
jacekelgda <jacekelgda@gmail.com>
2017-12-11 20:04:19 +0100

    Adding questions --wip #28

--- src/components/CreatePoll.vue
+++ src/components/CreatePoll.vue
@@ -1,17 +1,45 @@
 <template>
-  <div class="add-poll">
-    <h1>Create poll</h1>
+  <div class="create-poll">
     <el-form :inline="true" :model="poll" class="demo-form-inline">
       <el-form-item>
-        <el-input v-model="poll.first.value" placeholder="Poll name"></el-input>
+        <el-input v-model="poll.name" placeholder="Poll name"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">Activate</el-button>
+        <el-button type="primary" @click="createPoll">Activate</el-button>
       </el-form-item>
     </el-form>
+    <h3>TODO:Link should be generated on refresh</h3>
+    <p>This is your share link</p>
+
+    <el-form :inline="true" :model="question" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="question.first.value" placeholder="Option A"></el-input>
+      </el-form-item>
+      OR
+       <el-form-item>
+        <el-input v-model="question.second.value" placeholder="Option B"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="addQuestion">Publish</el-button>
+      </el-form-item>
+    </el-form>
+    <ul class="polls">
+      <li v-for="question in poll.questions">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <el-button style="float: right; padding: 3px" type="text">Remove question</el-button>
+          </div>
+          <el-row>
+            <ResultsNew></ResultsNew>
+          </el-row>
+        </el-card>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
+
+import ResultsNew from './ResultsNew';
 
 export default {
   name: 'CreatePoll',
@@ -19,7 +47,12 @@ export default {
     return {
       isLoading: true,
       poll: {
-        isOpen: true,
+        name: '',
+        url: '',
+        isActive: false,
+        questions: [],
+      },
+      question: {
         first: {
           value: '',
           votes: 0,
@@ -31,10 +64,16 @@ export default {
       },
     };
   },
+  components: {
+    ResultsNew,
+  },
   methods: {
-    onSubmit() {
+    createPoll() {
       // eslint-disable-next-line
-      console.log('submitted');
+      // console.log(polls)
+    },
+    addQuestion() {
+      this.poll.questions.push(this.question);
     },
   },
 };
@@ -42,5 +81,13 @@ export default {
 <style scoped>
 h1, h2, h3 {
   margin: 0;
+}
+.polls {
+  max-width: 600px;
+  margin: auto;
+  list-style: none;
+}
+.box-card {
+  margin: 10px;
 }
 </style>
---
+++ src/components/ResultsNew.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-loading="isLoading">
+    <div v-if="!this.isLoading">
+      <el-col :span="12">
+        <h2 class="grid-content">
+          {{ question.first.value }}
+        </h2>
+        <h3 class="grid-content">
+          {{ question.first.votes }} votes
+        </h3>
+      </el-col>
+      <el-col :span="12">
+        <h2 class="grid-content">
+          {{ question.second.value }}
+        </h2>
+        <h3 class="grid-content">
+          {{ question.second.votes }} votes
+        </h3>
+      </el-col>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'ResultsNew',
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+};
+</script>
+<style scoped>
+.polls {
+  max-width: 600px;
+  margin: auto;
+  list-style: none;
+}
+.bg-red {
+  background-color: #409EFF;
+}
+.bg-blue {
+  background-color: #67C23A;
+}
+.bar {
+  height: 20px;
+  margin-top: 5px;
+}
+.bar-container {
+  display: flex;
+  margin: 10px 0 30px;
+}
+</style>
