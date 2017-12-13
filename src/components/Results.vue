911993d25a2dc0b673b80778463080ab0c64d69a
jacekelgda <jacekelgda@gmail.com>
2017-12-13 15:56:10 +0100

    Colored results info

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,7 +2,7 @@
   <div v-loading="isLoading">
     <div v-if="!this.isLoading">
       <el-col :span="12">
-        <h2 class="grid-content">
+        <h2 class="grid-content blue">
           {{ activeQuestion.first.value }}
         </h2>
         <h3 class="grid-content">
@@ -10,7 +10,7 @@
         </h3>
       </el-col>
       <el-col :span="12">
-        <h2 class="grid-content">
+        <h2 class="grid-content green">
           {{ activeQuestion.second.value }}
         </h2>
         <h3 class="grid-content">
@@ -18,10 +18,10 @@
         </h3>
       </el-col>
       <el-col :span="24" class="bar-container">
-        <div v-bind:style="{width: activeQuestion.first.votes / (activeQuestion.first.votes + activeQuestion.second.votes) * 100 + '%' }" class="bar bg-red"/>
-        <div v-bind:style="{width: activeQuestion.second.votes / (activeQuestion.first.votes + activeQuestion.second.votes) * 100 + '%' }" class="bar bg-blue"/>
+        <div v-bind:style="{width: activeQuestion.first.votes / (activeQuestion.first.votes + activeQuestion.second.votes) * 100 + '%' }" class="bar bg-blue"/>
+        <div v-bind:style="{width: activeQuestion.second.votes / (activeQuestion.first.votes + activeQuestion.second.votes) * 100 + '%' }" class="bar bg-green"/>
       </el-col>
-      You voted for {{ activeQuestion[choice].value }}
+      You voted for <span v-bind:class="{ 'blue': isFirstChoice(activeQuestion, choice), 'green': isSecondChoice(activeQuestion, choice)}">{{ activeQuestion[choice].value }}</span>
     </div>
   </div>
 </template>
@@ -53,6 +53,14 @@ export default {
       },
     };
   },
+  methods: {
+    isFirstChoice(activeQuestion, choice) {
+      return activeQuestion[choice].value === activeQuestion.first.value;
+    },
+    isSecondChoice(activeQuestion, choice) {
+      return activeQuestion[choice].value === activeQuestion.second.value;
+    },
+  },
 };
 </script>
 <style scoped>
@@ -61,11 +69,17 @@ export default {
   margin: auto;
   list-style: none;
 }
-.bg-red {
-  background-color: #409EFF;
+.green {
+  color: #67C23A;
+}
+.blue {
+  color: #409EFF;
+}
+.bg-green {
+  background-color: #67C23A;
 }
 .bg-blue {
-  background-color: #67C23A;
+  background-color: #409EFF;
 }
 .bar {
   height: 20px;
